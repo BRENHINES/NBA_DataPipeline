f5c2ab355d04a6a2df38587bed1bc4ab78a29f0e
BRENHINES <yopasereina1971@gmail.com>
2025-04-18 16:02:53 +0200

    FEAT: Adding some definitions

--- nba_pipeline/nba_pipeline/definitions/definitions.py
+++ nba_pipeline/nba_pipeline/definitions/definitions.py
@@ -1,9 +1,20 @@
 from dagster import Definitions, load_assets_from_modules
-
-from nba_pipeline import assets  # noqa: TID252
+from nba_pipeline.nba_pipeline.assets import assets
+from nba_pipeline.nba_pipeline.jobs import jobs
+from nba_pipeline.nba_pipeline.schedules import schedule
+from nba_pipeline.nba_pipeline.sensors import sensors
+from nba_pipeline.nba_pipeline.resources.api_ressources import NBAAPIResource
+# from nba_pipeline.nba_pipeline.resources.db_ressources import db_resource
 
 all_assets = load_assets_from_modules([assets])
 
 defs = Definitions(
     assets=all_assets,
+    jobs=[jobs],
+    schedules=[schedule],
+    sensors=[sensors],
+    resources={
+        "nba_api": NBAAPIResource(api_key="env:NBA_API_KEY"),
+        # "db": db_resource
+    }
 )
